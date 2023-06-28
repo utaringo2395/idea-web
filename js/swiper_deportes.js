0ec6dfb50098f2f1e85804b5f197d6b356e4482d
Mariana M <mariana.macias2395@gmail.com>
2023-06-28 12:16:53 -0600

    Carrusel Deportes

---
+++ js/swiper_deportes.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiperDeportes", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  },
+});
